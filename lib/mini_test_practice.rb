ee30c9fa7c4edb3dbaa250b1e55b78b1a6584ace
Olivia-tou <olivia_tou@yahoo.co.jp>
2014-11-24 11:49:20 +0000

    update

---
+++ lib/mini_test_practice.rb
@@ -0,0 +1,13 @@
+require "mini_test_practice/version"
+
+module MiniTestPractice
+  class MyClass
+    def odd?(value)
+      if value % 2 == 0
+      return false
+      else
+      return true
+      end
+    end
+  end
+end
---
+++ test/test_mini_test_practice.rb
@@ -0,0 +1,18 @@
+require 'minitest_helper'
+
+class TestMiniTestPractice < MiniTest::Unit::TestCase
+  def setup
+    @my_class = MiniTestPractice::MyClass.new
+  end
+  
+  def test_that_it_has_a_version_number
+    refute_nil ::MiniTestPractice::VERSION
+  end
+  
+  def test_odd?
+    assert_equal true, @my_class.odd?(1)
+    assert_equal false, @my_class.odd?(100)
+    assert_equal true, @my_class.odd?(13)
+    assert_equal false, @my_class.odd?(0)
+  end
+end
