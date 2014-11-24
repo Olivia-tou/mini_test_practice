e1963e061a0f0bc66f64360ddd08c95d9993c7b8
Olivia-tou <olivia_tou@yahoo.co.jp>
2014-11-24 13:16:34 +0000

    Fix error

--- lib/mini_test_practice.rb
+++ lib/mini_test_practice.rb
@@ -1,16 +1,13 @@
 require "mini_test_practice/version"
 
 module MiniTestPractice
+# 6-2 odd?
   class MyClass
     def odd?(value)
-      if value % 2 == 0
-      return false
+      if value.is_a?(Integer) and value % 2 != 0
+      return true
       else
-        if value / 1 == value 
-        return true
-        else
-        return false
-        end
+      return false
       end
     end
   end
